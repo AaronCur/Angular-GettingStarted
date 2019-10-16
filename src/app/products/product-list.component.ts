bb3d5e01f21156384cec53603e20878027ed984f
Curry <aaron.m.curry84@gmail.com>
2019-10-16 16:25:11 +0100

    Added routing for multiple views and navbar

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -4,7 +4,6 @@ import { ProductService } from './products.service';
 
 
 @Component({
-  selector: 'pm-products',
   templateUrl: `./product-list.component.html`,
   styleUrls: ['./product-list.component.css']
 })
