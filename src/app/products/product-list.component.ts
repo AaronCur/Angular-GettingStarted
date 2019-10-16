d4dc8d2a6b86c133e59253a25d26c8fd31ffdba2
Curry <aaron.m.curry84@gmail.com>
2019-10-16 14:28:06 +0100

    Added products as a service

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from './products.service';
 
 
 @Component({
@@ -13,7 +14,7 @@ export class ProductListComponent implements OnInit {
     imageWidth: number = 50;
     imageMargin: number = 2;
     showImage: boolean = false;
-    _listFilter: string = 'cart';
+    _listFilter: string ;
 
     get listFilter(): string {
       return this._listFilter;
@@ -23,51 +24,11 @@ export class ProductListComponent implements OnInit {
       this.filteredProducts = this.listFilter ? this.performFilter(this.listFilter) : this.products;
     }
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            productId: 1,
-            productName: 'Leaf Rake',
-            productCode: 'GDN-0011',
-            releaseDate: 'March 19, 2019',
-            description: 'Leaf rake with 48-inch wooden handle.',
-            price: 19.95,
-            starRating: 3.2,
-            imageUrl: 'assets/images/leaf_rake.png'
-          },
-          {
-            productId: 2,
-            productName: 'Garden Cart',
-            productCode: 'GDN-0023',
-            releaseDate: 'March 18, 2019',
-            description: '15 gallon capacity rolling garden cart',
-            price: 32.99,
-            starRating: 4.2,
-            imageUrl: 'assets/images/garden_cart.png'
-          },
-          {
-            productId: 5,
-            productName: 'Hammer',
-            productCode: 'TBX-0048',
-            releaseDate: 'May 21, 2019',
-            description: 'Curved claw steel hammer',
-            price: 8.9,
-            starRating: 4.8,
-            imageUrl: 'assets/images/hammer.png'
-          },
-          {
-            productId: 8,
-            productName: 'Saw',
-            productCode: 'TBX-0022',
-            releaseDate: 'May 15, 2019',
-            description: '15-inch steel blade hand saw',
-            price: 11.55,
-            starRating: 3.7,
-            imageUrl: 'assets/images/saw.png'
-          }
-    ];
-    constructor(){
-      this.filteredProducts = this.products;
-      this.listFilter = 'cart';
+    products: IProduct[] = [];
+
+    constructor(private productService: ProductService){
+     
+
     }
     onRatingClicked(message: string): void{
       this.pageTitle = 'Product List: ' + message;
@@ -84,6 +45,7 @@ export class ProductListComponent implements OnInit {
     }
 
     ngOnInit(): void {
-      console.log('In Onit');
+      this.products = this.productService.getProducts();
+      this.filteredProducts = this.products;
     }
 }
--- src/app/shared/star.component.ts
+++ src/app/shared/star.component.ts
@@ -16,7 +16,6 @@ export class StarComponent implements OnChanges {
     ngOnChanges(): void{
         this.starWidth = this.rating * 75 / 5;
     }
-
     onClick():void{
         this.ratingClicked.emit('The rating ' + this.rating + ' was clicked!');
     }
