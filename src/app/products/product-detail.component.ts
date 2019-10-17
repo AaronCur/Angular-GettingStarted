8bbb8d1cb3af8d1d2dec30f2972ed0a27319c155
Curry <aaron.m.curry84@gmail.com>
2019-10-17 11:20:31 +0100

    Added product detail page

--- src/app/products/product-detail.component.ts
+++ src/app/products/product-detail.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 
 import {IProduct} from './product';
+import { ProductService } from './products.service';
 
 @Component({
   templateUrl: './product-detail.component.html',
@@ -9,26 +10,30 @@ import {IProduct} from './product';
 })
 export class ProductDetailComponent implements OnInit {
   pageTitle: string = 'Product Detail';
-  product: IProduct;
+  errorMessage: string = '';
+  product: IProduct | undefined;
+  
 
   constructor(private route: ActivatedRoute,
-              private router: Router) { }
+              private router: Router,
+              private productService: ProductService) { 
+
+              }
 
   ngOnInit() {
 
-    let id = +this.route.snapshot.paramMap.get('id');
-    this.pageTitle += ': ' + id;
-    this.product = {
-        productId: 1,
-        productName: 'Leaf Rake',
-        productCode: 'GDN-0011',
-        releaseDate: 'March 19, 2019',
-        description: 'Leaf rake with 48-inch wooden handle.',
-        price: 19.95,
-        starRating: 3.2,
-        imageUrl: 'assets/images/leaf_rake.png'
+    let param = +this.route.snapshot.paramMap.get('id');
+    if (param){
+      const id = +param;
+      this.getProduct(id);
+    }
+  }
 
-    };
+  getProduct(id: number){
+    this.productService.getProduct(id).subscribe({
+      next: product => this.product = product,
+      error: err => this.errorMessage = err
+    });
   }
 
   onBack(): void{
--- src/app/products/products.service.ts
+++ src/app/products/products.service.ts
@@ -1,44 +1,50 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
-import { catchError, tap} from 'rxjs/operators';
-
+import { catchError, tap, map } from 'rxjs/operators';
 
 import { IProduct } from './product';
 
-// Makes service available to entire application.
 @Injectable({
-    providedIn: 'root'
+  providedIn: 'root'
 })
-export class ProductService{
+export class ProductService {
+  // If using Stackblitz, replace the url with this line
+  // because Stackblitz can't find the api folder.
+  // private productUrl = 'assets/products/products.json';
+  private productUrl = 'api/products/products.json';
 
-    private productUrl = 'api/products/products.json';
+  constructor(private http: HttpClient) { }
 
-    constructor(private http: HttpClient){}
+  getProducts(): Observable<IProduct[]> {
+    return this.http.get<IProduct[]>(this.productUrl)
+      .pipe(
+        tap(data => console.log('All: ' + JSON.stringify(data))),
+        catchError(this.handleError)
+      );
+  }
 
-    getProducts(): Observable<IProduct[]>{
+  getProduct(id: number): Observable<IProduct | undefined> {
+    return this.getProducts()
+      .pipe(
+        map((products: IProduct[]) => products.find(p => p.productId === id))
+      );
+  }
 
-        return this.http.get<IProduct[]>(this.productUrl).pipe(
-            tap(data => console.log('All: ' + JSON.stringify(data))),
-            catchError(this.handleError)
-        );
+  private handleError(err: HttpErrorResponse) {
+    // in a real world app, we may send the server to some remote logging infrastructure
+    // instead of just logging it to the console
+    let errorMessage = '';
+    if (err.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      errorMessage = `An error occurred: ${err.error.message}`;
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      errorMessage = `Server returned code: ${err.status}, error message is: ${err.message}`;
     }
-
-    private handleError(err: HttpErrorResponse) {
-        // In a real word app, we send the server to some remote logging infrastructure
-        // Instead of just logging it to the console.
-
-        let errorMessage = '';
-        if (err.error instanceof ErrorEvent) {
-            // A client-side or network error occured. Handle it acccordingly.
-            errorMessage = 'An error occured: ${err.error.message}';
-        } else{
-            // The backend returned an unsuccessful response code.
-            // The reponse body may contain clues as to what went wrong.
-            errorMessage = 'Server returned code: ${err.status}, error message is: ${err.message}';
-        }
-        console.log(errorMessage);
-        return throwError(errorMessage);
-    }
+    console.error(errorMessage);
+    return throwError(errorMessage);
+  }
 
 }
